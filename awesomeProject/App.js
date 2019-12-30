bef58833bbcad19720c1b43f21e3ef246fea231e
aomine <aomine1745@gmail.com>
2019-12-30 11:47:49 -0500

    updated readme

--- awesomeProject/App.js
+++ awesomeProject/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
 
-export default function App() {
+import Loading from './components/shared/Loading.jsx'
+
+const App =  () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [value, setValue] = useState(0)
+  let loadingText = isLoading ? 'Loading...' : '¡Complete!'
+
+  useEffect(() => {
+    console.log('Use effect executed')
+  }, [isLoading])
+
+  setTimeout(() => {
+    setIsLoading(false)
+  }, 2000)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>funca esta wea dijo el Riac neitif :v</Text>
+      <Loading text={loadingText} />
+      {value === 0 && <Text>Puro cpp del tio pio</Text>}
+      <Text>Value: {value}</Text>
+      <Button title="Increse +1" onPress={() => setValue(value + 1)} />
+      <Button title="Decrease -1" onPress={() => setValue(value - 1)} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    color: '#212121',
+  }
+})
+
+export default App
---
+++ awesomeProject/components/shared/Loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Loading = ({ text }) => {
+  return (
+    <View>
+      <Text>{text}</Text>
+    </View>
+  )
+}
+
+export default Loading
