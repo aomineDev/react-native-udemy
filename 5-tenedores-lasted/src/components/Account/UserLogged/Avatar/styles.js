5f02ce11021c2d0f3099ae18090b23c7812962ed
aomine <aomine1745@gmail.com>
2020-07-01 11:16:04 -0500

    aver

--- 5-tenedores-lasted/src/components/Account/UserLogged/Avatar/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native'
-
-const styles = StyleSheet.create({
-  avatar: {
-    marginRight: 20
-  }
-})
-
-export default styles
--- 5-tenedores-lasted/src/components/Shared/Divider/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Divider } from 'react-native-elements'
-
-import styles from './styles'
-
-export default function DividerCompoent () {
-  return <Divider style={styles.divider} />
-}
